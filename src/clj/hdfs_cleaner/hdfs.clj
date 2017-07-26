d59b6b3df289073e47c063c429e82a8af68779f3
shihao <shisoftgenius@gmail.com>
2017-07-26 21:45:09 +0800

    enable shallow scan

--- src/clj/hdfs_cleaner/hdfs.clj
+++ src/clj/hdfs_cleaner/hdfs.clj
@@ -5,23 +5,34 @@
   (:import (com.factual.hdfs_cleaner HDFS)
            (org.apache.hadoop.fs Path FileStatus)))
 
-(defn scan** [^Path path]
+(def shallow-pattern [])
+
+(defn get-dir-size [^Path path]
+  (-> HDFS/dfs
+      (.getContentSummary path)
+      (.getLength)))
+
+(defn scan** [^Path path & [depth max-depth]]
   (map
     (fn [^FileStatus file]
-      (merge {:name (-> file (.getPath) (.getName))
-              ;:path (-> file (.getPath) (.toString))
-              :is-dir (.isDirectory file)
-              :last-modified (.getModificationTime file)
-              :replication (.getReplication file)}
-             (try
-               (let [sub-files (when (.isDirectory file) (scan** (.getPath  file)))]
-                 {:sub-files sub-files
-                  :size (if sub-files
-                          (reduce + (map :size sub-files))
-                          (.getLen file))})
-               (catch Exception e
-                 (log/error e "Scan failed for dir:" (-> file (.getPath) (.toString)))
-                 {:has-error true}))))
+      (let [path (.getPath file)]
+        (merge {:name (-> path (.getName))
+                :is-dir (.isDirectory file)
+                :last-modified (.getModificationTime file)
+                :replication (.getReplication file)}
+               (try
+                 (if (and (.isDirectory file)
+                          (or (and depth max-depth (>= depth max-depth))))
+                   {:size (get-dir-size path)
+                    :sub-files "..."}
+                   (let [sub-files (scan** path depth max-depth)]
+                     {:sub-files sub-files
+                      :size (if sub-files
+                              (reduce + (map :size sub-files))
+                              (.getLen file))}))
+                 (catch Exception e
+                   (log/error e "Scan failed for dir:" (-> path (.toString)))
+                   {:has-error true})))))
     (.listStatus HDFS/dfs path)))
 
 (defn scan* [^String path]
