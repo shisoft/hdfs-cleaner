6a31fd500457078f32d5f102dab26938cd85bf75
shihao <shisoftgenius@gmail.com>
2017-07-26 16:39:59 +0800

    spy

--- src/clj/hdfs_cleaner/hdfs.clj
+++ src/clj/hdfs_cleaner/hdfs.clj
@@ -1,9 +1,10 @@
 (ns hdfs-cleaner.hdfs
+  (:require [cluster-connector.utils.for-debug :refer [$ spy]])
   (:import (com.factual.hdfs_cleaner HDFS)
            (org.apache.hadoop.fs Path)))
 
 (defn scan [^String path]
   (let [start-time (System/currentTimeMillis)]
-    {:result (into [] (HDFS/scan (Path. (str hdfs-cleaner.config/hdfs-server path))))
+    {:result ($ into [] (HDFS/scan (Path. (str hdfs-cleaner.config/hdfs-server path))))
      :start-time start-time
      :end-time (System/currentTimeMillis)}))
